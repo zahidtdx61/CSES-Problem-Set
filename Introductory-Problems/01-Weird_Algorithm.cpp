3412d6b07394d7724cbe0429ab5274f23058b595
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2021-07-01 02:01:32 +0600

    Create 01-Weird_Algorithm.cpp

---
+++ Introductory-Problems/01-Weird_Algorithm.cpp
@@ -0,0 +1,23 @@
+//my solution link : https://cses.fi/paste/58f0c16f83525b0617976d/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long n;
+    cin >> n;
+    while(n!=1){
+        cout << n << " ";
+        if(n%2 == 0){
+            n = n/2;
+        }
+        else{
+            n = (3*n) +1 ;
+        }
+    }
+    cout << n << endl;
+    
+    return 0;
+}
+
